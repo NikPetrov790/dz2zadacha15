490ea62a469ba12e01bf16329be43f312839b2d6
Petrov Nikolay <nikolay.petrov99@mail.ru>
2022-08-15 15:01:28 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введи цифру, обозначающую день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+static void Checkweek (int number) {
+  if (number == 6 || number == 7) {
+  Console.WriteLine("(День является выходным) -> да");
+  }
+  else if (number < 1 || number > 7) {
+    Console.WriteLine("Это не является днем недели");
+  }
+  else Console.WriteLine("(Это не является выходным) -> нет");
+}
+
+Checkweek(number);
